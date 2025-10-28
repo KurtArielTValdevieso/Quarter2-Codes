9ce766d70e988ce0ccb925aebe0b45e75e00776c
KurtArielTValdevieso <kurtariel.valdevieso@smc.pshs.edu.ph>
2025-10-29 00:25:04 +0800

    This code will add all of the numbers from 1 to your age.

---
+++ age_adder.py
@@ -0,0 +1,11 @@
+age = int(input("Hi there! Please enter your age: "))
+
+while age <= 0:
+    print()
+    age = int(input("Enter your age (Must be a whole number): "))
+
+counter = 0
+for x in range (1, age + 1):
+    counter += x
+        
+print(f'The sum of all numbers from 1 to {age} is: {counter}')
